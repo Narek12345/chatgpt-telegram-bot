e3553fc4a1d61a47a3188fcf3eded7d8b822f20c
Narek <narekbayanduryan16@gmail.com>
2025-02-22 22:57:37 +0300

    Wrote Message model.

--- services/db/models.py
+++ services/db/models.py
@@ -19,4 +19,19 @@ class TelegramAccount(Base):
 	phone_number = Column(String(20), nullable=True)
 	is_admin = Column(Boolean, default=False)
 	registration_date = Column(DateTime, default=datetime.utcnow)
-	
+
+
+
+class Message(Base):
+	__tablename__ = "messages"
+
+	message_id = Column(BigInteger, primary_key=True, autoincrement=True)
+	message_type = Column(String(10), nullable=False)
+	message_text = Column(Text, nullable=False)
+	token_usage = Column(Integer, default=0)
+	is_bot = Column(Boolean, default=False)
+	created_at = Column(DateTime, default=datetime.utcnow)
+	telegram_id = Column(BigInteger, ForeignKey("telegram_accounts.telegram_id", on_delete="CASCADE"))
+
+	user = relationship("TelegramAccount", back_populates="messages")
+
