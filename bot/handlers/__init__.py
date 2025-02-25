d918358950367152932559f78965c3543beb788d
Narek <narekbayanduryan16@gmail.com>
2025-02-25 19:14:48 +0300

    Wrote simple handler for reset dialog history.

--- bot/handlers/__init__.py
+++ bot/handlers/__init__.py
@@ -1,5 +1,7 @@
 from .users.start import register_start_router
+from .users.dialog_history import register_dialog_history_router
 
 
 def register_all_routers(dp):
 	register_start_router(dp)
+	register_dialog_history_router(dp)
