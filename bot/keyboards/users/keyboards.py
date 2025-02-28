4715b1e6686d8a04ef2d7fa97cbe77694ff11c1d
Narek <narekbayanduryan16@gmail.com>
2025-03-01 00:47:57 +0300

    Updated user keyboard.

--- bot/keyboards/users/keyboards.py
+++ bot/keyboards/users/keyboards.py
@@ -3,8 +3,8 @@ from aiogram.utils.keyboard import ReplyKeyboardMarkup, KeyboardButton
 
 kb_list = [
 	[KeyboardButton(text="👤 Мой аккаунт"), KeyboardButton(text="🧹 Очистить историю")],
-	[KeyboardButton(text="⚙️ Настройки"), KeyboardButton(text="🎭 GPT - Роли")],
-	[KeyboardButton(text="🚀 Премиум"), KeyboardButton(text="📚 Для учебы и работы")],
+	[KeyboardButton(text="⚙️ Настройки"), KeyboardButton(text="💎 Бесплатные токены")],
+	[KeyboardButton(text="🚀 Премиум")],
 ]
 
 keyboard = ReplyKeyboardMarkup(keyboard=kb_list, resize_keyboard=True)
