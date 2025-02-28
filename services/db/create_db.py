c088f9b577b0836c28f93357282e42fffd281f64
Narek <narekbayanduryan16@gmail.com>
2025-02-28 21:43:41 +0300

    Wrote 'async_session_generator' function.

--- services/db/create_db.py
+++ services/db/create_db.py
@@ -1,4 +1,7 @@
 import os
+
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.ext.asyncio import create_async_engine
 
 
@@ -10,3 +13,9 @@ POSTGRESQL_DB_NAME = os.getenv("POSTGRESQL_DB_NAME")
 engine = create_async_engine(
 	f"postgresql+asyncpg://{POSTGRESQL_USERNAME}:{POSTGRESQL_PASSWORD}@{POSTGRESQL_DB_HOST}/{POSTGRESQL_DB_NAME}",
 )
+
+
+def async_session_generator():
+	return sessionmaker(
+		engine, class_=AsyncSession
+	)
