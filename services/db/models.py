fab1d227f1b0d720bca7da933f1a9a41516e58ed
Narek <narekbayanduryan16@gmail.com>
2025-02-28 22:44:20 +0300

    Wrote the 'Users' model.

---
+++ services/db/models.py
@@ -0,0 +1,22 @@
+from sqlalchemy import Column, String, Integer
+from sqlalchemy.orm import relationship
+
+from .create_db import Base, get_session
+
+
+
+class Users(Base):
+	__tablename__ = "users"
+
+	telegram_id = Column(Integer, primary_key=True, index=True)
+	firstname = Column(String(100))
+	lastname = Column(String(100))
+	username = Column(String(100))
+	phone_number = Column(String(20))
+	language_id = Column(Integer, ForeignKey('language.id'))
+	subscription_id = Column(Integer, ForeignKey('subscription.id'))
+	referall_link_id = Column(Integer, ForeignKey('referral_link.id'))
+
+	language = relationship("Languages")
+	subscription = relationship("Subscription", uselist=False)
+	referral_link = relationship("ReferralLink", uselist=False)
