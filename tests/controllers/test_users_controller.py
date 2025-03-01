631f010761a79e030fa0ea5536ec34d130e66dc7
Narek <narekbayanduryan16@gmail.com>
2025-03-02 01:10:39 +0300

    Renamed the test file.

---
+++ tests/controllers/test_users_controller.py
@@ -0,0 +1,19 @@
+import pytest
+import asyncio
+
+from services.db.controllers.users_model_controller import UsersModelController
+
+
+
+class TestUsersModelController():
+
+	@pytest.mark.asyncio
+	async def test_register(self):
+		test_data = {
+			"telegram_id": 1111111111,
+			"firstname": "firstname",
+			"lastname": "lastname",
+			"username": "username",
+			"phone_number": "+77777777777",
+		}
+		await UsersModelController.register(**test_data)
